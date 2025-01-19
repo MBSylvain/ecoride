eb40b30b87ef762a496df84e2d90151a81e1c9f9
MBSylvain <sylvain.mbeumou@gmail.com>
2025-01-19 17:57:00 +0100

    feature header base

--- ecoride/src/App.js
+++ ecoride/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <div>
         <h1>Bienvenue dans mon application</h1>
       </div>
