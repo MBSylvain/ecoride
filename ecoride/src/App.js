51531dc4ad83f4b7ba183f52c6981de69ae5b77d
MBSylvain <sylvain.mbeumou@gmail.com>
2025-01-22 09:03:03 +0100

    carte des avis client avec systeme de notation à etoile

--- ecoride/src/App.js
+++ ecoride/src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import CardAnnonce from "./components/Card-Annonce";
+import CardTrajet from "./components/Card-trajet";
+import CardAvis from "./components/CardAvisTrajets";
 
 function App() {
   return (
     <div className="App">
-      <CardAnnonce />
-      <div>
-        <h1>Bienvenue dans mon application</h1>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <CardAnnonce />
+        <CardTrajet />
+        <CardAvis text="emplacement du texte" />
+        <div>
+          <h1>Bienvenue dans mon application</h1>
+        </div>
       </div>
     </div>
   );
