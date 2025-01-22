a1a4ae65d0d1caebcc491a0fa04b36b4b73ec737
MBSylvain <sylvain.mbeumou@gmail.com>
2025-01-22 23:08:13 +0100

    Ajouter d'une card pour les filtre du site

--- ecoride/src/App.js
+++ ecoride/src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import CardAnnonce from "./components/Card-Annonce";
+import Cardfiltre from "./components/Card-filtre";
 
 function App() {
   return (
     <div className="App">
       <CardAnnonce />
+      <Header />
       <div>
         <h1>Bienvenue dans mon application</h1>
+        <Cardfiltre />
       </div>
     </div>
   );
