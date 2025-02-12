a15a87bda8ffe266a07152160c0414b90e563518
MBSylvain <sylvain.mbeumou@gmail.com>
2025-02-12 18:22:39 +0100

    Amélioration de la mise en page de la page de connexion en ajustant la largeur des champs de saisie et du bouton

--- ecoride/src/pages/login.js
+++ ecoride/src/pages/login.js
@@ -3,23 +3,23 @@ import Carlogin from "../assets/car-login.jpg";
 const LoginPage = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="flex flex-col items-start justify-center w-1/2 p-8">
+      <div className="flex flex-col justify-center w-1/2 p-8">
         <h1 className="text-4xl font-bold mb-4">Connexion</h1>
         <input
           type="email"
           placeholder="Email"
-          className="mb-4 p-3 w-full border border-gray-300 rounded-lg"
+          className="mb-4 p-3 w-80 border border-gray-300 rounded-lg"
         />
         <input
           type="password"
           placeholder="Mot de passe"
-          className="mb-6 p-3 w-full border border-gray-300 rounded-lg"
+          className="mb-6 p-3 w-80 border border-gray-300 rounded-lg"
         />
-        <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+        <button className="w-80 justify-center bg-blue-500 text-white rounded-lg hover:bg-blue-600">
           Login
         </button>
       </div>
-      <div className="w-1/2">
+      <div className="w-1/2 ">
         <img
           src={Carlogin}
           alt="Connexion"
