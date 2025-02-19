463cb3df565477252115619171022a74e6663643
MBSylvain <sylvain.mbeumou@gmail.com>
2025-02-19 18:39:11 +0100

    installation de react router

--- ecoride/src/App.js
+++ ecoride/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Accueil from "./pages/accueil";
 
 function App() {
   return (
     <div className="App">
       <div>
         <Header />
+        <Accueil />
+        <Footer />
       </div>
     </div>
   );
