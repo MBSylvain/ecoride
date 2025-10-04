d3dd108c1de97e8a3ecc1cbd566936e9406f9f49
MBsylvain <sylvain.mbeumou@gmail.com>
2025-10-04 16:31:32 +0200

    fix: amélioration de la vérification d'authentification utilisateur

--- ecoride/src/features/checkAuth.js
+++ ecoride/src/features/checkAuth.js
@@ -4,13 +4,23 @@ const checkAuth = async () => {
   try {
     const response = await axios.get("http://localhost/api/Controllers/checkAuth.php", {
       headers: { "Content-Type": "application/json" },
-        withCredentials: true, // Nécessaire pour envoyer les cookies de session
+      withCredentials: true,
     });
 
-    return response.data.isAuthenticated; // Retourne true si l'utilisateur est authentifié
+    if (response.data.isAuthenticated) {
+      // Stockage des informations de session dans le localStorage
+      localStorage.setItem("utilisateur_id", response.data.utilisateur_id);
+      localStorage.setItem("user.email", response.data.email);
+      localStorage.setItem("user.role", response.data.role);
+      return true;
+    } else {
+      localStorage.clear();
+      return false;
+    }
   } catch (err) {
     console.error("Erreur lors de la vérification de l'authentification :", err);
-    return false; // Retourne false en cas d'erreur
+    localStorage.clear();
+    return false;
   }
 };
 
