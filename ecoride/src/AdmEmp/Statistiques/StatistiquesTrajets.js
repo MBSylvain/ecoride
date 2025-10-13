79bb870bb6f00bfeb95b45262a9328e0b55d38a5
MBsylvain <sylvain.mbeumou@gmail.com>
2025-10-14 00:37:51 +0200

    Mise à jour: StatistiquesTrajets.js

--- ecoride/src/AdmEmp/Statistiques/StatistiquesTrajets.js
+++ ecoride/src/AdmEmp/Statistiques/StatistiquesTrajets.js
@@ -22,6 +22,8 @@ const StatistiquesTrajets = () => {
         <div className="w-full max-w-2xl px-2 mx-auto my-8">
             <h3 className="text-lg font-bold">Trajets</h3>
             <p>{stats?.total_trajets ?? "—"} trajets</p>
+           <div className="flex flex-row gap-2 mx-auto md:flex-col xl:flex-row"> 
+            <section className="w-full p-4 mb-8 bg-white shadow rounded-xl">
             <BarChart width={500} height={300} data={stats?.trajets_par_jour || []}>
                 <XAxis dataKey="jour" />
                 <YAxis />
@@ -29,11 +31,15 @@ const StatistiquesTrajets = () => {
                 <Legend />
                 <Bar dataKey="total" fill="#82ca9d" />
             </BarChart>
+            </section>
+            <section className="w-full p-4 mb-8 bg-white shadow rounded-xl">
             <p>{stats?.trajets_annules ?? "—"} trajets annulés</p>
             <p>{stats?.trajets_problemes ?? "—"} trajets problématiques</p>
             <p>{stats?.trajets_ecologiques ?? "—"} trajets écologiques</p>
             <p>{stats?.trajets_en_cours ?? "—"} trajets en cours</p>
             <p>{stats?.trajets_termines ?? "—"} trajets terminés</p>
+            </section>
+            </div>
         </div>
     );
 };
