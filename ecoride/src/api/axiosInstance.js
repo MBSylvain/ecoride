0f7d87f966edd005e8499c5b518c3e3eccfddf6b
MBSylvain <155894547+MBSylvain@users.noreply.github.com>
2025-11-10 14:33:13 +0000

    fix: update baseURL in axiosInstance configuration

--- ecoride/src/api/axiosInstance.js
+++ ecoride/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://api-ecride-production.up.railway.app/Controllers/",
+  baseURL: "https://ecoride-sylvains-projects-15c39aad.vercel.app/Controllers/",
   // Vous pouvez ajouter ici des headers ou d'autres options si besoin
 });
 
